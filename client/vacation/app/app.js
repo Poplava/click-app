d313775d28fb8a35292479f400bf050ca5aea09e
Igor Poplavsky <igor.poplava@gmail.com>
2015-07-03 11:20:48 +0300

    vacation

--- client/vacation/app/app.js
+++ client/vacation/app/app.js
@@ -3,15 +3,19 @@ var app = angular.module('App', []);
 app.controller('MainCtrl', MainCtrl);
 
 function MainCtrl($scope, $interval) {
-    var finish = moment('2015-07-03 19:00');
+    var finish;
+
+    $scope.init = function(date) {
+        finish = moment(date);
+        $interval($scope.update, 1000);
+        $scope.update();
+    };
 
     $scope.update = function() {
         $scope.timerH = finish.diff(moment(), 'hours');
         $scope.timerM = finish.diff(moment(), 'minutes') % 60;
         $scope.timerS = finish.diff(moment(), 'seconds') % 60;
     };
-
-    $interval($scope.update, 10);
 }
 
 MainCtrl.$inject = ['$scope', '$interval'];
